c040d1f231b0b110ed2f863063d80fce2c724647
Donschki <donvonkrog@gmail.com>
2022-05-09 10:02:09 +0200

    Adding files to Fiori cakes

---
+++ Fiori Cakes - OLD VERSION/Script.js
@@ -0,0 +1,17 @@
+
+
+
+const menuIcon = document.querySelector
+(".hamburger-menu");
+const navbar = document.querySelector (".nav-bar");
+
+menuIcon.addEventListener("click", ()=> {
+    navbar.classList.toggle("change");
+
+});
+
+
+
+  
+                    
+
---
+++ Script.js
@@ -0,0 +1,10 @@
+
+const menuIcon = document.querySelector
+("#logo-img");
+const nav = document.querySelector ("nav");
+
+menuIcon.addEventListener("click", ()=> {
+    nav.classList.toggle("change");
+
+});
+
